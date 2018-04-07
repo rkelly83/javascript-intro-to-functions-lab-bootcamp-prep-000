5493f0bbd9d9bc638d2b4f9f0d1f26bc8e269cb2
Robert Kelly <robert.kelly83@gmail.com>
2018-04-07 16:08:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  var uppercase = "HELLO"
+  var mixedcase = "I love you, Grandma."
+  
+  if (string.toLowerCase() === lowercase){
+  console.log("I can't hear you!")
+}
+else if (string.toUpperCase() === uppercase){
+  console.log ("YES INDEED!")
+}
+else if (string.to)
+}
+
+
+
+
+}
