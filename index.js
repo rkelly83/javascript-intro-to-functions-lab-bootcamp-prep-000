c9a18363107f585f7099ce89bbdfde523995ee5b
Robert Kelly <robert.kelly83@gmail.com>
2018-04-07 16:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO"
   var mixedcase = "I love you, Grandma."
   
-  if (string.toLowerCase() === lowercase){
+  if ( === lowercase){
   console.log("I can't hear you!")
 }
 else if (string.toUpperCase() === uppercase){
